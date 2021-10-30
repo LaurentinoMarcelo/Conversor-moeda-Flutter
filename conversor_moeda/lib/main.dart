d9b6f4f052ee07fa562606687467b0b6b66d31c2
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2021-10-30 19:37:50 -0300

    projetinho finalizado

--- conversor_moeda/lib/main.dart
+++ conversor_moeda/lib/main.dart
@@ -9,6 +9,10 @@ const request = "https://api.hgbrasil.com/finance/quotations?key=351bee89";
 void main() async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white
+    ),
   ));
 }
 
@@ -20,6 +24,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final realControler = TextEditingController();
+  final dolarControler = TextEditingController();
+  final euroControler = TextEditingController();
+
+  double dolar = 0.0;
+  double euro = 0.0;
+
+  void _clearAll(){
+    realControler.text = "";
+    dolarControler.text = "";
+    euroControler.text = "";
+  }
+
+  void _realChange(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarControler.text = (real/dolar).toStringAsFixed(2);
+    euroControler.text = (real/euro).toStringAsFixed(2);
+  }
+
+  void _dolarChange(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realControler.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroControler.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChange(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realControler.text = (euro * this.euro).toStringAsFixed(2);
+    dolarControler.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,23 +84,37 @@ class _HomeState extends State<Home> {
             case ConnectionState.none:
             case ConnectionState.waiting:
               return Center(
-                  child: Text("Carregando Dados...",
-                      style: TextStyle(
-                          color: Colors.amber,
-                          fontSize: 25.0),
-                      textAlign: TextAlign.center,)
-              );
+                  child: Text(
+                "Carregando Dados...",
+                style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                textAlign: TextAlign.center,
+              ));
             default:
-              if(snapshot.hasError){
+              if (snapshot.hasError) {
                 return Center(
-                    child: Text("Erro ao Carregar Dados :(",
-                      style: TextStyle(
-                          color: Colors.amber,
-                          fontSize: 25.0),
-                      textAlign: TextAlign.center,)
+                    child: Text(
+                  "Erro ao Carregar Dados :(",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ));
+              } else {
+                dolar = snapshot.data!["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data!["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
+                      buildTextField("Reais", "R\$", realControler, _realChange),
+                      Divider(),
+                      buildTextField("Dolares", "U\$", dolarControler, _dolarChange),
+                      Divider(),
+                      buildTextField("Euros", "€", euroControler, _euroChange),
+                    ],
+                  ),
                 );
-              }else{
-                return Container (color: Colors.green,);
               }
           }
         },
@@ -61,8 +123,24 @@ class _HomeState extends State<Home> {
   }
 }
 
-
 Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController c, Function f){
+  return TextField(
+    controller: c,
+      decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.amber),
+          border: OutlineInputBorder(),
+          prefixText: prefix
+      ),
+      style:TextStyle(
+          color: Colors.amber,fontSize: 25.0,
+      ),
+    onChanged: f,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+  );
 }
